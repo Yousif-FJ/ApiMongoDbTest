87783cbedaa5df16898bb32eed9698fee0744e21
Yousef-FJ <47510107+Yousif-FJ@users.noreply.github.com>
2023-01-14 20:50:56 +0300

    Remove old connections string

--- ApiMongoDB/Database/AppDb.cs
+++ ApiMongoDB/Database/AppDb.cs
@@ -5,9 +5,10 @@ namespace ApiMongoDB.Database;
 public class AppDb
 {
     public IMongoDatabase MongoDatabase { get; }
-    public AppDb()
+    public AppDb(IConfiguration configuration)
 	{
-        var settings = MongoClientSettings.FromConnectionString("mongodb+srv://mongo:U99jCSqUj8Y9l5LS@cluster0.dawys7q.mongodb.net/?retryWrites=true&w=majority");
+        var connectionString = configuration["MongoDbTestConnectionString"];
+        var settings = MongoClientSettings.FromConnectionString(connectionString);
         settings.ServerApi = new ServerApi(ServerApiVersion.V1);
         var client = new MongoClient(settings);
             
